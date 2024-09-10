f1023a485eb42d21c95973146bc1b19308920751
Aryaman <aryaman@Aryamans-MacBook-Pro.local>
2024-09-10 16:30:16 -0500

    Updating kush's webpage

--- src/pages/Kush.py
+++ src/pages/Kush.py
@@ -1,1 +1,20 @@
+import streamlit as st
+from datetime import datetime
 
+
+st.title("Kush ")
+
+txt = st.write(
+
+"Monday and Wednesday at 7:00 AM "
+
+    "Where : SRC Aerobics Studio "
+
+)
+
+
+st.header("Class timings : ")
+
+st.write(
+    
+)
